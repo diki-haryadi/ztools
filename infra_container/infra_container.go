d46409997ed6f59a4155e19dcf61a3980851de9a
Diki Haryadi <dk.harryadi@gmail.com>
2024-11-21 08:52:56 +0700

    refactor(infra_container):  change New IC to chaining function

--- infra_container/infra_container.go
+++ infra_container/infra_container.go
@@ -26,49 +26,22 @@ type IContainer struct {
 	EchoHttpServer echoHttp.ServerInterface
 	KafkaWriter    *kafkaProducer.Writer
 	KafkaReader    *kafkaConsumer.Reader
+	DownFns        []func()
+	Down           func()
 }
 
-func NewIC(ctx context.Context) (*IContainer, func(), error) {
+func (ic *IContainer) ICDown() *IContainer {
 	var downFns []func()
 	down := func() {
 		for _, df := range downFns {
 			df()
 		}
 	}
+	ic.Down = down
+	return ic
+}
 
-	se := sentry.Init(sentry.ClientOptions{
-		Dsn:              config.BaseConfig.Sentry.Dsn,
-		TracesSampleRate: 1.0,
-		EnableTracing:    config.IsDevEnv(),
-	})
-	if se != nil {
-		_ = fmt.Errorf("can not initialize sentry with error:  %s", se)
-	}
-	downFns = append(downFns, func() {
-		sentry.Flush(2 * time.Second)
-	})
-
-	grpcServerConfig := &grpc.Config{
-		Port:        config.BaseConfig.Grpc.Port,
-		Host:        config.BaseConfig.Grpc.Host,
-		Development: config.IsDevEnv(),
-	}
-	grpcServer := grpc.NewGrpcServer(grpcServerConfig)
-	downFns = append(downFns, func() {
-		grpcServer.GracefulShutdown()
-	})
-
-	echoServerConfig := &echoHttp.ServerConfig{
-		Port:     config.BaseConfig.Http.Port,
-		BasePath: "/api/v1",
-		IsDev:    config.IsDevEnv(),
-	}
-	echoServer := echoHttp.NewServer(echoServerConfig)
-	echoServer.SetupDefaultMiddlewares()
-	downFns = append(downFns, func() {
-		_ = echoServer.GracefulShutdown(ctx)
-	})
-
+func (ic *IContainer) ICPostgres(ctx context.Context) *IContainer {
 	pg, err := postgres.NewConnection(ctx, &postgres.Config{
 		Host:    config.BaseConfig.Postgres.Host,
 		Port:    config.BaseConfig.Postgres.Port,
@@ -78,46 +51,157 @@ func NewIC(ctx context.Context) (*IContainer, func(), error) {
 		SslMode: config.BaseConfig.Postgres.SslMode,
 	})
 	if err != nil {
-		return nil, down, fmt.Errorf("can not connect to database using sqlx with error: %s", err)
+		return nil
 	}
-	downFns = append(downFns, func() {
-		pg.Close()
+	ic.Postgres = pg
+	ic.DownFns = append(ic.DownFns, func() {
+		ic.Postgres.Close()
 	})
+	return ic
+}
 
+func (ic *IContainer) ICGrpc(ctx context.Context) *IContainer {
+	grpcServerConfig := &grpc.Config{
+		Port:        config.BaseConfig.Grpc.Port,
+		Host:        config.BaseConfig.Grpc.Host,
+		Development: config.IsDevEnv(),
+	}
+	ic.GrpcServer = grpc.NewGrpcServer(grpcServerConfig)
+	ic.DownFns = append(ic.DownFns, func() {
+		ic.GrpcServer.GracefulShutdown()
+	})
+	return ic
+}
+
+func (ic *IContainer) ICEcho(ctx context.Context) *IContainer {
+	echoServerConfig := &echoHttp.ServerConfig{
+		Port:     config.BaseConfig.Http.Port,
+		BasePath: "/api/v1",
+		IsDev:    config.IsDevEnv(),
+	}
+	ic.EchoHttpServer = echoHttp.NewServer(echoServerConfig)
+	ic.EchoHttpServer.SetupDefaultMiddlewares()
+	ic.DownFns = append(ic.DownFns, func() {
+		_ = ic.EchoHttpServer.GracefulShutdown(ctx)
+	})
+	return ic
+}
+
+func (ic *IContainer) ICKafka(ctx context.Context) *IContainer {
 	var kw *kafkaProducer.Writer
 	var kr *kafkaConsumer.Reader
 
-	if config.BaseConfig.Kafka.Enabled {
-		kwc := &kafkaProducer.WriterConfig{
-			Brokers:      config.BaseConfig.Kafka.ClientBrokers,
-			Topic:        config.BaseConfig.Kafka.Topic,
-			RequiredAcks: kafka.RequireAll,
-		}
-		kw = kafkaProducer.NewKafkaWriter(kwc)
-		downFns = append(downFns, func() {
-			_ = kw.Client.Close()
-		})
-
-		krc := &kafkaConsumer.ReaderConfig{
-			Brokers: config.BaseConfig.Kafka.ClientBrokers,
-			Topic:   config.BaseConfig.Kafka.Topic,
-			GroupID: config.BaseConfig.Kafka.ClientGroupId,
-		}
-		kr = kafkaConsumer.NewKafkaReader(krc)
-		downFns = append(downFns, func() {
-			_ = kr.Client.Close()
-		})
+	kwc := &kafkaProducer.WriterConfig{
+		Brokers:      config.BaseConfig.Kafka.ClientBrokers,
+		Topic:        config.BaseConfig.Kafka.Topic,
+		RequiredAcks: kafka.RequireAll,
 	}
+	ic.KafkaWriter = kafkaProducer.NewKafkaWriter(kwc)
+	ic.DownFns = append(ic.DownFns, func() {
+		_ = kw.Client.Close()
+	})
 
-	ic := &IContainer{
+	krc := &kafkaConsumer.ReaderConfig{
+		Brokers: config.BaseConfig.Kafka.ClientBrokers,
+		Topic:   config.BaseConfig.Kafka.Topic,
+		GroupID: config.BaseConfig.Kafka.ClientGroupId,
+	}
+	ic.KafkaReader = kafkaConsumer.NewKafkaReader(krc)
+	ic.DownFns = append(ic.DownFns, func() {
+		_ = kr.Client.Close()
+	})
+	return ic
+}
+
+func (ic *IContainer) NewIC(ctx context.Context) (*IContainer, func(), error) {
+	//var downFns []func()
+	//down := func() {
+	//	for _, df := range downFns {
+	//		df()
+	//	}
+	//}
+	se := sentry.Init(sentry.ClientOptions{
+		Dsn:              config.BaseConfig.Sentry.Dsn,
+		TracesSampleRate: 1.0,
+		EnableTracing:    config.IsDevEnv(),
+	})
+	if se != nil {
+		_ = fmt.Errorf("can not initialize sentry with error:  %s", se)
+	}
+	ic.DownFns = append(ic.DownFns, func() {
+		sentry.Flush(2 * time.Second)
+	})
+
+	//grpcServerConfig := &grpc.Config{
+	//	Port:        config.BaseConfig.Grpc.Port,
+	//	Host:        config.BaseConfig.Grpc.Host,
+	//	Development: config.IsDevEnv(),
+	//}
+	//grpcServer := grpc.NewGrpcServer(grpcServerConfig)
+	//downFns = append(downFns, func() {
+	//	ic.GrpcServer.GracefulShutdown()
+	//})
+
+	//echoServerConfig := &echoHttp.ServerConfig{
+	//	Port:     config.BaseConfig.Http.Port,
+	//	BasePath: "/api/v1",
+	//	IsDev:    config.IsDevEnv(),
+	//}
+	//echoServer := echoHttp.NewServer(echoServerConfig)
+	//echoServer.SetupDefaultMiddlewares()
+	//downFns = append(downFns, func() {
+	//	_ = ic.EchoHttpServer.GracefulShutdown(ctx)
+	//})
+
+	//pg, err := postgres.NewConnection(ctx, &postgres.Config{
+	//	Host:    config.BaseConfig.Postgres.Host,
+	//	Port:    config.BaseConfig.Postgres.Port,
+	//	User:    config.BaseConfig.Postgres.User,
+	//	Pass:    config.BaseConfig.Postgres.Pass,
+	//	DBName:  config.BaseConfig.Postgres.DBName,
+	//	SslMode: config.BaseConfig.Postgres.SslMode,
+	//})
+	//if err != nil {
+	//	return nil, down, fmt.Errorf("can not connect to database using sqlx with error: %s", err)
+	//}
+	//downFns = append(downFns, func() {
+	//	ic.Postgres.Close()
+	//})
+
+	//var kw *kafkaProducer.Writer
+	//var kr *kafkaConsumer.Reader
+
+	//if config.BaseConfig.Kafka.Enabled {
+	//	kwc := &kafkaProducer.WriterConfig{
+	//		Brokers:      config.BaseConfig.Kafka.ClientBrokers,
+	//		Topic:        config.BaseConfig.Kafka.Topic,
+	//		RequiredAcks: kafka.RequireAll,
+	//	}
+	//	kw = kafkaProducer.NewKafkaWriter(kwc)
+	//	downFns = append(downFns, func() {
+	//		_ = kw.Client.Close()
+	//	})
+	//
+	//	krc := &kafkaConsumer.ReaderConfig{
+	//		Brokers: config.BaseConfig.Kafka.ClientBrokers,
+	//		Topic:   config.BaseConfig.Kafka.Topic,
+	//		GroupID: config.BaseConfig.Kafka.ClientGroupId,
+	//	}
+	//	kr = kafkaConsumer.NewKafkaReader(krc)
+	//	downFns = append(downFns, func() {
+	//		_ = kr.Client.Close()
+	//	})
+	//}
+
+	nic := &IContainer{
 		Config:         config.BaseConfig,
 		Logger:         logger.Zap,
-		Postgres:       pg,
-		GrpcServer:     grpcServer,
-		EchoHttpServer: echoServer,
-		KafkaWriter:    kw,
-		KafkaReader:    kr,
+		Postgres:       ic.Postgres,
+		GrpcServer:     ic.GrpcServer,
+		EchoHttpServer: ic.EchoHttpServer,
+		KafkaWriter:    ic.KafkaWriter,
+		KafkaReader:    ic.KafkaReader,
 	}
 
-	return ic, down, nil
+	return nic, ic.Down, nil
 }
